8d7a32bf07d00dc57e3eb9fa62d7a59fd7947c09
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2026-02-17 21:46:15 +0530

    npm

--- Backend/server/src/index.js
+++ Backend/server/src/index.js
@@ -3,14 +3,20 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import helmet from 'helmet'
 import cors from "cors";
-import router from "./routes/healthcheck.routes.js";
+import router from "../routes/healthcheck.routes.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,          // disable CSP if you don't serve HTML
+    crossOriginEmbedderPolicy: false,      // relax if needed for API
+    crossOriginResourcePolicy: { policy: "cross-origin" }, // allow cross-origin resource sharing
+  })
+);
 app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
 app.use("/api/v1/healthcheck", router);
 
