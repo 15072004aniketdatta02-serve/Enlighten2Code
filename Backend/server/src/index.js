3987240a84c033baceba97b1505ff96fc7b72f76
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2026-02-24 20:08:11 +0530

    helmet

--- Backend/server/src/index.js
+++ Backend/server/src/index.js
@@ -22,7 +22,7 @@ app.use(morgan("combined", { stream: morganStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
+app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
 app.use("/api/v1/healthcheck", router);
 app.use("/api/v1/auth", authRoute);
 
