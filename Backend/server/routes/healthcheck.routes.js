fbc0cd50bf46909363df48e25869537bf1354719
ANIKET_DATTA <15.07.2004aniketdatta02@gmail.com>
2026-04-10 11:15:02 +0530

    deletion

--- Backend/server/routes/healthcheck.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "../controllers/healthcheck.controllers.js";
-const router = Router();
-
-// router.get("/healthcheck", healthCheck);
-router.route("/").get(healthCheck);
-
-
-export default router;
--- Backend/server/routes/runCode.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middlewares/auth.middlewares.js";
-import { runCode } from "../controllers/runCode.controller.js";
-
-const runCodeRoutes = express.Router();
-
-runCodeRoutes.post("/", authMiddleware, runCode);
-
-export default runCodeRoutes;
